eeca594b1f0ce17af726eb14642962f6d75b70e2
vikren <viktor.rehnberg@gmail.com>
2021-06-30 12:55:23 +0200

    Add super ticks

--- src/torchutils/visualize.py
+++ src/torchutils/visualize.py
@@ -5,7 +5,7 @@ from torch import nn
 
 import matplotlib.pyplot as plt
 from matplotlib import ticker
-from matplotlib.axis import XTick, YTick
+from matplotlib.axis import XAxis, XTick, YTick
 
 
 def center_ticks(axis, ticks, ticklabels):
@@ -24,13 +24,61 @@ def center_ticks(axis, ticks, ticklabels):
         tick.tick2line.set_markersize(0)
         if isinstance(tick, XTick):
             tick.label1.set_horizontalalignment('center')
-        elif isinstance(tick, XTick):
-            tick.label1.set_verticalalignment('middle')
+        elif isinstance(tick, YTick):
+            tick.label1.set_verticalalignment('center')
 
 
-def add_sublabels(axis, ticks, ticklabels, level):
-    pass
-    #TODO
+def add_sublabels(axis, ticks, ticklabels, level, previous_label="", loc=None):
+    assert len(ticks) - 1 == len(ticklabels)
+    if loc is None:
+        loc = "bottom" if isinstance(axis, XAxis) else "left"
+    
+    # Add sublabel lines
+    ax = axis.axes
+    sgn = 1 if loc in ["right", "top"] else -1
+    offset = 0.1 * sgn * level + (1 if loc in ["right", "top"] else 0)
+    if isinstance(axis, XAxis):
+        trans = ax.get_xaxis_transform()
+        get_xy = lambda pos: (pos, offset)
+        ha = "center"
+        va = "top" if loc=="bottom" else "bottom"
+        lim = ax.get_xlim()
+        dx = (max(ax.get_xlim()) - min(ax.get_xlim())) / 100
+        get_plot_xy = lambda start, stop, offset: ([start + dx, stop - dx], [offset, offset])
+        rotation = "horizontal"
+    else:
+        trans = ax.get_yaxis_transform()
+        get_xy = lambda pos: (offset, pos)
+        ha = "right" if loc=="left" else "left"
+        va = "center"
+        lim = ax.get_ylim()
+        dy = (max(ax.get_ylim()) - min(ax.get_ylim())) / 100
+        get_plot_xy = lambda start, stop, offset: ([offset, offset], [start + dy, stop - dy])
+        rotation = "vertical"
+
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+    for start, stop, ticklabel in zip(ticks[:-1], ticks[1:], ticklabels):
+        pos = (start + stop) / 2
+        ax.annotate(ticklabel, xy=get_xy(pos), xycoords=trans, ha=ha, va=va, rotation=rotation)
+        ax.plot(*get_plot_xy(start, stop, offset), color="grey", transform=trans, clip_on=False)
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+
+    def ra(a):
+        if a == "left":
+            return "right"
+        if a == "right":
+            return "left"
+        if a == "bottom":
+            return "top"
+        if a == "top":
+            return "bottom"
+        return a
+    
+    pos = (max(lim) - min(lim)) / 2
+    ax.annotate(previous_label, xy=get_xy(pos), transform=trans, ha=ra(ha), va=ra(va), rotation=rotation)
+    
 
 def tensorshow(tensor, xdims, ydims, fignum=None):
     assert hasattr(tensor, "names")
@@ -55,21 +103,32 @@ def tensorshow(tensor, xdims, ydims, fignum=None):
 
     # Add hierarchical axes labels
     def add_labels(axis, dims, level=0):
-        if level > len(dims):
+        if level >= len(dims):
             return
             
         ticklabels = list(reversed(list(zip(*product(*[range(tensor.size(dim)) for dim in dims])))))[level]
-        ticklabels = list(map(str, ticklabels))
+        ticklabels = np.asarray(list(map(str, ticklabels)))
         n_ticks = len(ticklabels) + 1
         ticks = np.arange(n_ticks) - 0.5
         if level==0:
             center_ticks(axis, ticks, ticklabels)
         else:
-            add_sublabels(axis, ticks, ticklabels, level, previous_label=dims[-level])
+            mask = np.hstack([[True], ticklabels[:-1]!=ticklabels[1:]])
+            mticks = np.hstack([ticks[:-1][mask], ticks[-1]])
+            mticklabels = np.array(ticklabels)[mask]
+            add_sublabels(
+                axis,
+                mticks,
+                mticklabels,
+                level,
+                previous_label=dims[-level],
+                loc="top" if isinstance(axis, XAxis) else "left",
+            )
         
         if level == len(dims):
             axis.set_label(dims[0])
 
+        return add_labels(axis, dims, level=level + 1)
 
         
     for axis, dims in zip([ax.xaxis, ax.yaxis], [xdims, ydims]):
